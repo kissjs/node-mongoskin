d65cf6b9d91f575f58d3d040cdf9b9183da8f7be
guilin <guileen@gmail.com>
2011-11-12 13:59:57 +0800

    * add gridfs support

--- History.md
+++ History.md
@@ -1,3 +1,7 @@
+0.2.0 / 2011-11-06
+==================
+  * add SkinDB.gridfs
+
 0.1.3 / 2011-05-24
 ==================
   * add SkinCollection.removeById
---
+++ examples/gridfs.js
@@ -0,0 +1,15 @@
+var mongoskin = require('../lib/mongoskin');
+
+var db = mongoskin.db('localhost/test');
+
+db.gridfs().open('test.txt', 'w', function(err, gs) {
+    gs.write('blablabla', function(err, reply) {
+        gs.close(function(err, reply){
+            db.gridfs().open('test.txt', 'r' ,function(err, gs) {
+                gs.read(function(err, reply){
+                    console.log(reply);
+                });
+            });
+        });
+    });
+});
