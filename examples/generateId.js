9dd82ba42598e4ab4b728f4fe7989adec29fbbf9
Lin Gui <guileen@gmail.com>
2012-03-01 04:15:27 +0800

    add generateId.js

---
+++ examples/generateId.js
@@ -0,0 +1,33 @@
+var mongo = require('../index.js')
+  , redis = require('redis').createClient()
+  , shorten = require('shorten')(redis)
+  , async = require('async')
+  , db = mongo.db('localhost/test')
+  ;
+
+db.bind('system.js');
+db.bind('user');
+
+function log(err) {
+  if(err) {
+    console.log(err.stack);
+  }
+}
+
+function createUser(user, callback) {
+
+  async.waterfall([
+      function(fn) {
+        shorten.nextId('user', fn);
+      }
+    , function(uid, fn) {
+        user.uid = uid;
+        db.user.save(user, fn);
+      }
+  ], callback);
+
+}
+
+for(var i = 0; i<10; i++) {
+  createUser({name: 'user' + i}, log);
+}
