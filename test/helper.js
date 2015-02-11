a812ea09005ee60268cf7b3c05e3c39a607ab542
perfaram <perfaram@windowslive.com>
2015-02-11 18:14:19 +0100

    Added tests for isObjectID

--- test/helper.js
+++ test/helper.js
@@ -21,3 +21,14 @@ describe('helper.id()', function () {
     });
 });
 
+describe('helper.isObjectID()', function () {
+    it('should return false when ID is not valid hex', function () {
+        var id = '54da050b878fd5d462672bea';
+        var isID = helper.isObjectID(id);
+        isID.should.be.typeof(boolean);
+        isID.should.equal(true);
+        id = 'h4da050b878fd5d462672bea';
+        isID.should.be.typeof(boolean);
+        isID.should.equal(false);
+    });
+});
